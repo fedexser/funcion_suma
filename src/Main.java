d475a240511a8524d3154577d32121f6e94ef438
Fede <fedeserra@gmail.com>
2023-03-24 18:06:06 -0300

    primer commit funcion suma

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        int resultado;
+        resultado=suma(a,b,c);
+        System.out.println(resultado);
+
+    }
+
+
+   public static int suma(int a, int b, int c){
+       return a + b + c;
+   }
+
+}
